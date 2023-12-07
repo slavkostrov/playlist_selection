92849df3aa4845d482b06a34fcf22f8a092d7309
Vyacheslav Kostrov <slavkostrov@gmail.com>
2023-12-07 20:22:02 +0000

    [ADD] Dumpers for audio

--- src/downloading/__init__.py
+++ src/downloading/__init__.py
@@ -1,4 +1,4 @@
 """Audio downloading package."""
-from .downloader import YouTubeDownloader
+from .downloader import YouTubeDownloader, S3AudioDumper
 
-__all__ = ["YouTubeDownloader"]
+__all__ = ["YouTubeDownloader", "S3AudioDumper"]

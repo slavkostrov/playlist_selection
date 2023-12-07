c95413b9e9e7739a10dc6e81273b160ad0948435
Nikita <saraevnik0909@gmail.com>
2023-12-08 02:13:40 +0300

    [ADD] basic metrics and baseline notebook

---
+++ src/metrics/__init__.py
@@ -0,0 +1,17 @@
+from .metrics import (  # noqa
+    CorrectGenreShare,
+    CorrectMultipleGenreShare,
+    CorrectAlbumShare,
+    CorrectArtistShare,
+    YearMeanDiff,
+    SoundParametersDiff,
+)
+
+__all__ = [
+    "CorrectGenreShare",
+    "CorrectMultipleGenreShare",
+    "CorrectAlbumShare",
+    "CorrectArtistShare",
+    "YearMeanDiff",
+    "SoundParametersDiff",
+]

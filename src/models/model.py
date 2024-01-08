eeceea3dcd0a240c468fd69cda85db438de3d38c
Daniil Ganych <dganych@yandex.ru>
2024-01-08 18:33:35 +0300

    enhaces and fixes

--- src/models/model.py
+++ src/models/model.py
@@ -5,7 +5,7 @@ from abc import ABC, abstractmethod
 from pandas.core.api import DataFrame as DataFrame
 
 from sklearn.base import BaseEstimator
-from sklearn.compose import ColumnTransformer
+from sklearn.compose import ColumnTransformer, make_column_transformer, make_column_selector
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.pipeline import make_pipeline
@@ -20,20 +20,19 @@ DROP_COLUMNS = [
     "track_id"
 ]
 
+
 class BaseModel(ABC):
     """Base model class."""
 
     @abstractmethod
-    def preprocessor(self, **preprocess_params) -> DataFrame:
+    def get_preprocessor(self, **preprocess_params) -> DataFrame:
         """Preproccess data."""
-
         return NotImplementedError()
 
     
     @abstractmethod
     def open(self) -> BaseEstimator:
         """Open model."""
-
         return NotImplementedError()
     
 
@@ -42,7 +41,6 @@ class KnnModel(BaseModel):
 
     def __init__(
         self,
-        dataset: pd.DataFrame,
         k_neighbors: int = 4,
         metric: str = "manhattan", 
         n_components: int = 141, 
@@ -57,89 +55,72 @@ class KnnModel(BaseModel):
 
         :return:
         """
-
-        self.dataset = dataset
         self.k_neighbors = k_neighbors
         self.metric = metric
         self.n_components = n_components
 
 
-    def prettifier(self) -> DataFrame:
+    def prettify(self, dataset) -> DataFrame:
         """
         Pretify incoming dataset.
 
         Useful only for meta dataset from S3Dataset.
         """
-
-        self.dataset = self.dataset.drop(columns=DROP_COLUMNS).dropna(subset="track_name")
-        if self.dataset["genres"].dtype == "object":
-            self.dataset["genres"] = self.dataset["genres"].apply(eval)
-        if self.dataset["artist_name"].dtype == "object":
-            self.dataset["artist_name"] = self.dataset["artist_name"].apply(eval).apply(set)
+        dataset = dataset.drop(columns=DROP_COLUMNS).dropna(subset="track_name")
+        if dataset["genres"].dtype == "object":
+            dataset["genres"] = dataset["genres"].apply(eval)
+        if dataset["artist_name"].dtype == "object":
+            dataset["artist_name"] = dataset["artist_name"].apply(eval).apply(set)
         else:
-            self.dataset["artist_name"] = self.dataset["artist_name"].apply(set)
+            dataset["artist_name"] = dataset["artist_name"].apply(set)
 
-        bad_date = ~self.dataset["album_release_date"].str.fullmatch("\d{4}-\d{2}-\d{2}")
-        self.dataset.loc[bad_date, "album_release_date"] = self.dataset.loc[bad_date, "album_release_date"].str[:10]
-        self.dataset["album_year"] = pd.to_datetime(self.dataset["album_release_date"]).dt.year
-        self.dataset[["explicit", "is_local"]] = self.dataset[["explicit", "is_local"]].astype("int")
+        bad_date = ~dataset["album_release_date"].str.fullmatch("\d{4}-\d{2}-\d{2}")
+        dataset.loc[bad_date, "album_release_date"] = dataset.loc[bad_date, "album_release_date"].str[:10]
+        dataset["album_year"] = pd.to_datetime(dataset["album_release_date"]).dt.year
+        dataset[["explicit", "is_local"]] = dataset[["explicit", "is_local"]].astype("int")
+        model_columns = dataset.columns.difference(["album_name", "artist_name", "track_name", "album_release_date",  "genres"])
 
-        model_columns = self.dataset.columns.difference(["album_name", "artist_name", "track_name", "album_release_date",  "genres"])
-        self.model_dataset = self.dataset[model_columns]
-
-        return self.model_dataset
+        return dataset[model_columns]
 
 
-    def preprocessor(self) -> ColumnTransformer:
+    def get_preprocessor(self, model_dataset) -> ColumnTransformer:
         """
         Preproccess features.
 
         Encode with OneHotEncoder every object feature.
         Scale number features with StandardScaler, replace NaN with SimpleImputer.
         """
-
-        num_features = self.model_dataset.select_dtypes("number").columns
-        cat_features = self.model_dataset.select_dtypes("object").columns
-
-        cat_encoder = OneHotEncoder(handle_unknown="ignore")
-        imputer = SimpleImputer()
-        scaler = StandardScaler()
-        preprocessor = ColumnTransformer([
-            ("num_transformer", make_pipeline(imputer, scaler), num_features),
-            ("cat_transformer", cat_encoder, cat_features),
-        ])
+        categorical_transformer = OneHotEncoder(handle_unknown="ignore", sparse_output=False)
+        numeric_transformer = make_pipeline(SimpleImputer(), StandardScaler())
+        
+        preprocessor = make_column_transformer(
+            (numeric_transformer, 
+             make_column_selector(dtype_include=np.number)),
+            (categorical_transformer, 
+             make_column_selector(dtype_include=object))
+        ).set_output(transform="pandas")
 
         return preprocessor
     
 
-    def decomposer(self) -> PCA:
+    def get_decomposer(self) -> PCA:
         """
         Decompose dataset.
         """
-
-        pca = PCA(n_components=self.n_components)
-        
-        return pca
+        decomposer = PCA(n_components=self.n_components).set_output(transform="pandas")
+        return decomposer
     
 
-    def open(self) -> BaseEstimator:
+    def open(self, dataset) -> BaseEstimator:
         """
         Trains and opens KNN model.
         """
+        self.preprocessor = self.get_preprocessor(self.prettify(dataset))
+        self.preprocessor.fit(self.prettify(dataset))
+        preprocessed_dataset = self.preprocessor.transform(self.prettify(dataset))
         
-        model_dataset = self.prettifier()
-
-        preprocessor = self.preprocessor().fit(model_dataset)
-        preprossed_dataset = pd.DataFrame(
-            preprocessor.transform(model_dataset).toarray(), 
-            columns=preprocessor.get_feature_names_out()
-        )
-        
-        pca = self.decomposer()
-        pca_dataset = pd.DataFrame(
-            pca.fit_transform(preprossed_dataset), 
-            columns=[f"comp_{i}" for i in range(1, self.n_components+1)]
-        )
+        self.decomposer = self.get_decomposer().fit(preprocessed_dataset)
+        pca_dataset = self.decomposer.transform(preprocessed_dataset)
 
         self.model = NearestNeighbors(
             n_neighbors=self.k_neighbors, 

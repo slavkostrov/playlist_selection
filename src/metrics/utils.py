fae662ee6de40a367be084d48fb4754a1d05507e
Nikita <saraevnik0909@gmail.com>
2024-01-11 00:34:59 +0300

    [FIX] metrics for usage with multiple predictions

---
+++ src/metrics/utils.py
@@ -0,0 +1,25 @@
+"""Utils for metrics calculation."""
+
+import numpy as np
+import pandas as pd
+
+
+def prepare_for_metric(
+        df: pd.DataFrame,
+        columns: list[str] | str,
+        neighbors_indexes: np.array,
+) -> tuple[np.array, np.array]:
+    """Prepare data for metrics evaluation.
+
+    Args:
+        df: DataFrame to take data from
+        columns: features to take from dataframe
+        neighbors_indexes: indexes to take after model inference
+    """
+    y_true = df[columns].to_numpy()
+    y_pred = np.take(y_true, neighbors_indexes, axis=0)
+
+    if y_pred.shape[-1] == 1:
+        y_pred = y_pred.reshape(*y_pred.shape[:-1])
+
+    return y_true, y_pred

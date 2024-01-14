5afc68e4f74be07337094466ccc4d2b81bc1b914
Vyacheslav Kostrov <slavkostrov@gmail.com>
2024-01-14 19:58:04 +0000

    update

--- app/main.py
+++ app/main.py
@@ -37,7 +37,7 @@ async def model_lifespan(app: FastAPI):
     MODEL = model_class.open(
         bucket_name=os.environ["PLAYLIST_SELECTION_S3_BUCKET_NAME"],
         model_name=os.environ["PLAYLIST_SELECTION_MODEL_NAME"],
-        profile_name=os.environ["PLAYLIST_SELECTION_S3_PROFILE_NAME"],
+        profile_name=os.environ.get("PLAYLIST_SELECTION_S3_PROFILE_NAME"),
     )
     LOGGER.info("Model %s loaded, version: %s", model_class, os.environ["PLAYLIST_SELECTION_MODEL_NAME"])
     yield

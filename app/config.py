e927f59276138db5f21416848869ff130e67ddf0
Vyacheslav Kostrov <slavkostrov@gmail.com>
2024-03-12 20:14:34 +0000

    [ADD] config.py

---
+++ app/config.py
@@ -0,0 +1,28 @@
+import functools
+
+import pydantic
+import pydantic_settings
+
+
+class Settings(pydantic_settings.BaseSettings):
+    model_config = pydantic_settings.SettingsConfigDict(env_file=".env_db", env_file_encoding="utf-8")
+
+    # Postgres
+    PGUSER: str = "user"
+    PGPASSWORD: pydantic.SecretStr = "pass"  # type: ignore[assignment]
+    PGHOST: str = "localhost"
+    PGPORT: int = 5432
+    PGDATABASE: str = "{{ project_slug }}"
+
+    @property
+    def pg_dsn_revealed(self) -> str:
+        return f"postgresql+asyncpg://{self.PGUSER}:{self.PGPASSWORD.get_secret_value()}@{self.PGHOST}:{self.PGPORT}/{self.PGDATABASE}"
+
+    @property
+    def pg_dsn(self) -> str:
+        return f"postgresql+asyncpg://{self.PGUSER}:{self.PGPASSWORD}@{self.PGHOST}:{self.PGPORT}/{self.PGDATABASE}"
+
+
+@functools.lru_cache
+def get_settings() -> Settings:
+    return Settings()

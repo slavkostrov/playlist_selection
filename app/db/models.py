564159e8b2c169d883001ef62c7b8bb160c0e69e
Vyacheslav Kostrov <slavkostrov@gmail.com>
2024-03-16 10:36:37 +0000

    [ADD] __repr__ for models

--- app/db/models.py
+++ app/db/models.py
@@ -49,6 +49,10 @@ class User(Base):
 
     requests: orm.Mapped[list["Request"] | None] = orm.relationship(back_populates="user")
 
+    def __repr__(self) -> str:
+        """Representation for user."""
+        return f"User(uid='{self.uid}', created_at='{self.created_at}')"
+
 
 class Playlist(Base):
     """Model with information about generated playlists."""
@@ -61,6 +65,10 @@ class Playlist(Base):
     request: orm.Mapped["Request"] = orm.relationship(back_populates="playlist")
     songs: orm.Mapped[list["Song"]] = orm.relationship(secondary=playlist_to_song_table)
 
+    def __repr__(self) -> str:
+        """Representation for playlist."""
+        return f"Playlist(uid='{self.uid}', name='{self.name}', created_at='{self.created_at}')"
+
 
 class Song(Base):
     """Model with information about songs."""
@@ -70,6 +78,10 @@ class Song(Base):
     name: orm.Mapped[str] = orm.mapped_column(nullable=False)
     artist_name: orm.Mapped[sa.ARRAY] = orm.mapped_column(sa.ARRAY(sa.String(64)), nullable=False)
     link: orm.Mapped[str] = orm.mapped_column(nullable=False)
+
+    def __repr__(self) -> str:
+        """Representation for song."""
+        return f"Song(name='{self.name}', artist_name='{self.artist_name}', link='{self.link}')"
 
 
 class Request(Base):
@@ -86,6 +98,9 @@ class Request(Base):
     playlist_uid: orm.Mapped[uuid.UUID | None] = orm.mapped_column(sa.ForeignKey("playlist.uid"))
     playlist: orm.Mapped["Playlist"] = orm.relationship(back_populates="request")
 
+    def __repr__(self) -> str:
+        """Representation for request."""
+        return f"Request(uid='{self.uid}', user_uid='{self.user_uid}', created_at='{self.created_at}')"
 
 # TODO: un update ставить плейлист готов
 # TODO: обновлять статус по таймауту

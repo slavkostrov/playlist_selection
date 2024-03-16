39e594d51ca74d6639fbff233e713a662fb76bfa
Vyacheslav Kostrov <slavkostrov@gmail.com>
2024-03-16 10:30:32 +0000

    [ADD] link column

--- app/db/models.py
+++ app/db/models.py
@@ -68,6 +68,7 @@ class Song(Base):
     id: orm.Mapped[int] = orm.mapped_column(primary_key=True)
     name: orm.Mapped[str] = orm.mapped_column(nullable=False)
     artist_name: orm.Mapped[sa.ARRAY] = orm.mapped_column(sa.ARRAY(sa.String(64)), nullable=False)
+    link: orm.Mapped[str] = orm.mapped_column(nullable=False)
 
 
 class Request(Base):
